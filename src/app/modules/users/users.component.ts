f31f21f266c64f119e639da4c1d9311bc92b014e
amine mountij <mountijamine.dev@gmail.com>
2024-09-04 13:48:26 +0100

    create user table

--- src/app/modules/users/users.component.ts
+++ src/app/modules/users/users.component.ts
@@ -1,13 +1,31 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { user } from '../../mock-api/common/user/data';
 
 @Component({
     selector     : 'users',
     standalone   : true,
     templateUrl  : './users.component.html',
     encapsulation: ViewEncapsulation.None,
+    imports : [TableModule],
 })
 export class UsersComponent
 {
+    users = [
+        {
+            "firstname": "jawad",
+            "lastname" : "ezzahraoui",
+            "profile": "web developer",
+            "department": "WL UK" 
+        },
+        {
+            "firstname": "amine",
+            "lastname" : "ezzahraoui",
+            "profile": "web developer",
+            "department": "WL UK" 
+        },
+
+    ]
     /**
      * Constructor
      */
